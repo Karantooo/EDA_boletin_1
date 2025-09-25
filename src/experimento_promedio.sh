9a25851f458848aa5fc96446413c994d572dd269
karantooo <ctomasalvarezn@gmail.com>
2025-09-25 19:28:58 -0300

    feat: experimentos promedio

--- src/experimento_promedio.sh
+++ src/experimento_promedio.sh
@@ -3,7 +3,7 @@
 # Cambiar directorio de trabajo al padre
 cd ..
 
-OUTPUT="resultados_experimentos/resultados_experimento_1.csv"
+OUTPUT="resultados_experimentos/resultados_experimento_promedio.csv"
 
 # Añadir encabezado al archivo
 echo "n,tiempo_lineal,tiempo_binaria,tiempo_galopante" > $OUTPUT
@@ -16,6 +16,6 @@ for ((r=1; r<=REPS; r++)); do
     echo "Repetición $r de $REPS"
     # Bucle de tamaños
     for ((n=0; n<=1000000; n+=10000)); do
-        ./experimento_1.out $n >> $OUTPUT
+        ./experimento_1.out $n -a >> $OUTPUT
     done
 done
